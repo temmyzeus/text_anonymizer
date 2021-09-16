61710910598c99073c6e9559a8c72f9a77eeb8f6
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2021-09-16 13:37:24 +0100

    Change `GET` request to `POST` request

--- api.py
+++ api.py
@@ -7,17 +7,17 @@ api = FastAPI()
 
 @api.get("/")
 def get_root():
-    return "Text Anonymizer Roor API"
+    return "Text Anonymizer Root API"
 
 
-@api.get("/ent_pos/{text}")
+@api.post("/ent_pos/{text}")
 def get_pos(text: str):
     ent_pos = get_entity_positions(text)
     return {
         'entity_positions': ent_pos
     }
 
-@api.get('/x_anonymize/{text}')
+@api.post('/x_anonymize/{text}')
 def get_anonymized(text: str):
     anonymized_text = x_anonymize(text)
     return {
