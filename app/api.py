da04c5c3a25c43d8ea398f922664c8849a7fc0ab
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2021-09-24 16:53:26 +0100

    Use Data Models fore Requests and Response

--- app/api.py
+++ app/api.py
@@ -30,19 +30,23 @@ def get_root():
     return "Text Anonymizer Root API"
 
 
-@api.post("/ent_pos/{text}")
-def get_pos(text: str):
-    ent_pos = get_entity_positions(text)
-    return {
-        'entity_positions': ent_pos
-    }
+@api.post("/ent_pos/")
+def get_pos(text: Text):
 
-@api.post('/x_anonymize/{text}')
-def get_anonymized(text: str):
-    anonymized_text = x_anonymize(text)
-    return {
-        'anonymized_text': anonymized_text
-    }
+    ent_pos = get_entity_positions(text.text)
+    return EntityPositions(
+        len=len(ent_pos),
+        entities=ent_pos
+    )
+
+@api.post('/x_anonymize/')
+def get_anonymized(text: Text):
+
+    anonymized_text = x_anonymize(text.text)
+    return AnonymizedText(
+        len=len(anonymized_text),
+        text=anonymized_text
+    )
 
 if __name__ == '__main__':
     uvicorn.run('api:api', host='0.0.0.0', port=8000)
